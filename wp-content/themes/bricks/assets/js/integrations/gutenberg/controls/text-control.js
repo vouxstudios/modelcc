420a25a87d373e8453160686f9a93596aaf59b14
Voux Studios <vouxstudios@gmail.com>
2026-03-12 20:33:06 -0400

    Add files via upload

---
+++ wp-content/themes/bricks/assets/js/integrations/gutenberg/controls/text-control.js
@@ -0,0 +1,27 @@
+/**
+ * Create text control matching ControlText.vue
+ */
+function createBricksTextControl(property, props) {
+	try {
+		const { TextControl } = window.wp.components
+		const { createElement } = window.wp.element
+
+		return createElement(TextControl, {
+			__next40pxDefaultSize: true,
+			__nextHasNoMarginBottom: true,
+			key: property.id,
+			label: property.label,
+			value: props.attributes[property.id] || '',
+			placeholder: property.placeholder || property.default,
+			onChange: function (value) {
+				try {
+					const newAttributes = {}
+					newAttributes[property.id] = value
+					props.setAttributes(newAttributes)
+				} catch (error) {}
+			}
+		})
+	} catch (error) {
+		return null
+	}
+}
